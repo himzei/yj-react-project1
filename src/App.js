eb24c25cea1a3ed74a0faf275dcfa9ab08105692
himzei <himzei@gmail.com>
2023-05-03 13:36:46 +0900

    global nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { GlobalStyles } from "./styles";
+
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  
+  display: flex; 
+  justify-content: center;
+`;
+const GlobalNav = styled.div`
+  width: 1000px; 
+  
+  display: flex;
+  justify-content: space-between; 
+  
+  
+  height: 80px;
+  align-items: center;
+`
+const SmallNav = styled.div`
+  display: flex; 
+  justify-content: space-between;
+  div{
+   padding: 0 15px;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyles />
+    <Container>
+      <GlobalNav>
+        <div>logo</div>
+        <SmallNav>
+          <div>업체소개</div>
+          <div>포트폴리오</div>
+          <div>사진</div>
+          <div>게시판</div>
+          <div>연락</div>
+        </SmallNav>
+      </GlobalNav>
+    </Container>
+    </>
   );
 }
 
